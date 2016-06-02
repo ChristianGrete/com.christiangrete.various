b7fd8f331cf0ae339b42761735ffd39a86945cf6
Christian Grete <webmaster@christiangrete.com>
2016-06-02 13:25:11 +0200

    fix(src/js/cjs): use function expression - also add comment

--- src/js/cjs/module-wide-scope.js
+++ src/js/cjs/module-wide-scope.js
@@ -4,13 +4,19 @@
  * and https://github.com/shichuan/javascript-patterns/blob/master/general-patterns/access-to-global-object.html
  */
 
-const context = (
-    () => this || ( 1, eval )('this')
+'use strict';
+
+const _context = (
+    // Has to be a classical function expression rather than an arrow function,
+    // otherwise the `this` value would have been binded lexically.
+    function () {
+      return this || ( 1, eval )('this');
+    }
   )();
 
 Object.assign(
-  context,
-  { foo: 'bar' }
+  _context,
+  { _foo: 'bar' }
 );
 
-console.log( foo ); // Should be `'bar'`
+console.log( _foo ); // Should be `'bar'`
